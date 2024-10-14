8cadb79614c858157138f50a1d911f2d944741cf
starcradle101 <dreamerdev12@gmail.com>
2024-10-14 16:04:48 +0900

    fix: webpack .map 무시 옵션 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.map$/, // .map 파일을 대상으로 함
+      use: 'ignore-loader', // .map 파일을 무시하는 로더 사용
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
